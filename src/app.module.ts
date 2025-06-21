ad9870f91963766e90c12f9df507b92aca2484d2
Artem Malikov <artemmalikov742@gmail.com>
2025-06-21 10:06:50 +0000

    feat/8699g3ufd-set-up-env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { envValidationSchema } from './config/env-validation.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.ENV_PATH,
+      validationSchema: envValidationSchema,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
