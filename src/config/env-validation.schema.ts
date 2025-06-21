bf5abe6a6e5b5d9c182d7fe1fa057a79fad9ade2
Artem Malikov <artemmalikov742@gmail.com>
2025-06-21 17:48:32 +0000

    feat/8699g3uaw-set-up-docker-compose

--- src/config/env-validation.schema.ts
+++ src/config/env-validation.schema.ts
@@ -9,4 +9,12 @@ export const envValidationSchema = Joi.object({
   ALLOWED_METHODS: Joi.string().required(),
   ALLOWED_ORIGINS: Joi.string().required(),
   ALLOWED_HEADERS: Joi.string().required(),
+  // Database configuration
+  POSTGRES_USER: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
+  POSTGRES_DB: Joi.string().required(),
+  POSTGRES_HOST: Joi.string().default('localhost'),
+  POSTGRES_PORT: Joi.number().default(5432),
+  // Redis configuration
+  REDIS_PORT: Joi.number().default(6379),
 });
