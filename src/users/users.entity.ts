90284282996e1451c88588edc0db9cf9eb2b23a0
Anton Duchenko <anton.duchenko2@gmail.com>
2025-06-22 11:06:15 +0000

    implemented TypeOrm

---
+++ src/users/users.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: false, name: 'is_confirmed' })
+  isConfirmed: boolean;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+  ) {}
+
+  getAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+}
