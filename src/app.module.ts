47a2c4e24a1f9f762f42cb31c6b3869498bc6683
artem8746 <artemmalikov742@gmail.com>
2025-06-23 16:11:22 +0200

    fix: add version to controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { SentryModule } from '@sentry/nestjs/setup';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { PostgresDataSource } from './database/data-source';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -64,6 +65,7 @@ import { PostgresDataSource } from './database/data-source';
       },
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
